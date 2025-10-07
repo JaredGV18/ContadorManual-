127fd92b1cbfaa7ccc61829ec0a0db09cd776c4b
Jared Daniel Gonzalez Vicente <22470168@campeche.tecnm.mx>
2025-10-07 10:33:01 -0600

    Version de prueba subida de trabajo moviles

--- App.js
+++ App.js
@@ -1,44 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import TextButton from './components/TextButton';
 
 
 export default function App() {
+  const [conteo, setConteo] = useState(0)
+  
+  const Contar = () => setConteo(conteo + 1)
+  const Reiniciar = () => setConteo(0)
+
   return (
+    <View 
+          style={styles.container}>
     <View
-    style={styles.container}>
-      <Text
-      style={styles.conteoStyle}>
-        Conteo: 
-        </Text>
-      <View style={styles.conteoContainer}>
-              <Text style={styles.conteoTexStyle}> 0 </Text>
-      </View>
-      <View>
-         <Button title='CONTAR'/>
-         <Button title='REINICIAR'/>
-      </View>
-      <StatusBar style="auto" />
+    style={styles.conteoStyle}>
+    <Text style={{fontSize:24}}>
+    Conteo:</Text>    
+    </View>
+       
+    <View
+    style={styles.conteoContainer}>
+    <Text style={styles.conteoText}
+    >{conteo}</Text>
+    </View>
+
+    <View
+    style={styles.controlsContainer}>
+      <TextButton title ='Contar' onTab={Contar}/>
+      <TextButton title ='Reiniciar' onTab={Reiniciar}/>
+    
+    </View>
+
+    <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
+    backgroundColor: '#ffff',
     marginVertical: 40,
     marginHorizontal: 16,
   },
-conteoContainer: {
-  marginVertical: 48,
-  borderColor: '#636161',
-  borderWidth: 2,
-  alignItems: 'center',
-  padding: 24,
+  conteoContainer:{
+    flex: 0,
+    marginVertical: 48,
+     borderColor: 'gray',
+     borderWidth: 2,
+     alignItems: 'center',
+     padding: 16,
+
+    },
+
+  conteoStyle: {
+    flex: 0,
+
+    fontSize:24,
   },
-conteoStyle: {
-  fontSize: 24,
-  },
-  conteoTexStyle: {
+
+  conteoText:{
+    flex: 0,
     fontSize: 36,
-    color: 'red',
+    color: 'red'
+
+  },
+
+  controlsContainer: {
+    flex: 0,
+    flexDirection:'row',
+
+    justifyContent: 'space-evenly',
   }
+
+
 });
